ea4a246571cf58e7d8e5899062e70ac92eecd8c6
Byron Lopez <bj@localhost.localdomain>
2020-05-12 09:07:05 -0600

    uiss builds

---
+++ build/precache-manifest.8bee2cd19e9b37539c1b8182dc963957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8124a2330892da16d8d5e807ae7a48c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d90ac04c953be8c27bfc",
+    "url": "/static/css/main.5f724c1b.chunk.css"
+  },
+  {
+    "revision": "ae62ac01a9e10f46bd78",
+    "url": "/static/js/2.33111a29.chunk.js"
+  },
+  {
+    "revision": "d90ac04c953be8c27bfc",
+    "url": "/static/js/main.a665e15b.chunk.js"
+  },
+  {
+    "revision": "aa5dd861c3df341bfb4f",
+    "url": "/static/js/runtime-main.0fda84ed.js"
+  }
+]);
